e15cab806ad5c0330d749372d4718f3074e9ba67
Akshat Shrivastava <akshatrules04@gmail.com>
2024-01-03 14:37:13 -0500

    Created Movies project

---
+++ src/main/java/dev/akshat/movies/Movie.java
@@ -0,0 +1,27 @@
+package dev.akshat.movies;
+
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "Movies") // let the framework know that this class represents each document in the movies collection
+@Data // takes care of all getter setter and to string methods.
+@All
+public class Movie {
+    @Id // let the framework know that this property should be treated as a unique identifier for each movie in database.
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    private List<String> reviewIDs;
+
+
+}
+
---
+++ src/main/java/dev/akshat/movies/MoviesApplication.java
@@ -0,0 +1,22 @@
+package dev.akshat.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// annotation are used to the java what this class does.
+@SpringBootApplication
+@RestController // another annotation that lets the framework know that this is a rest api controller
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+//	@GetMapping("/root") // annother annotation that lets the framwork know to GET requests onto specific handler methods.
+//	public String apiRoot(){
+//		return "Hello, World";
+//	}
+
+}
