edb361c76dca3f84e852a5073f8e7966bf914b92
Akshat Shrivastava <akshatrules04@gmail.com>
2024-01-03 14:41:39 -0500

    added constructors

--- src/main/java/dev/akshat/movies/Movie.java
+++ src/main/java/dev/akshat/movies/Movie.java
@@ -1,6 +1,8 @@
 package dev.akshat.movies;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,6 +11,8 @@ import java.util.List;
 
 @Document(collection = "Movies") // let the framework know that this class represents each document in the movies collection
 @Data // takes care of all getter setter and to string methods.
+@AllArgsConstructor // annotation that takes all the private variable as constructors
+@NoArgsConstructor // constructor that takes arguments
 public class Movie {
     @Id // let the framework know that this property should be treated as a unique identifier for each movie in database.
     private ObjectId id;
