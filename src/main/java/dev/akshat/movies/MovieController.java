a46c6890a86d47d06ca541eef58e1b02cef85c42
Akshat Shrivastava <akshatrules04@gmail.com>
2024-01-03 16:45:38 -0500

    modified MovieController

--- src/main/java/dev/akshat/movies/MovieController.java
+++ src/main/java/dev/akshat/movies/MovieController.java
@@ -1,17 +1,23 @@
 package dev.akshat.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
     @GetMapping
-    public ResponseEntity<String> getAllMovies(){
+    public ResponseEntity<List<Movie>> getAllMovies(){
         // RespnseEntity represent the whole HTTP response. Can use to fully configure the HTTP reponse.
-        return new ResponseEntity<String>("All Movies", HttpStatus.OK);
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(),HttpStatus.OK);
     }
 }
