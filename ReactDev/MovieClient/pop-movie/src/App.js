688b6e60688ca7932f625c74056f62f558a020e8
Akshat Shrivastava <akshatrules04@gmail.com>
2024-01-04 15:10:49 -0500

    Changes

--- ReactDev/MovieClient/pop-movie/src/App.js
+++ ReactDev/MovieClient/pop-movie/src/App.js
@@ -5,8 +5,20 @@ import {useState,useEffect} from 'react';
 function App() {
 
   const[movies,setMovies] = useState();
-  
 
+  const getMovies = async () =>{
+    try {
+      const response = await api.get("/api/v1/movies");
+      console.log(response.data);
+      setMovies(response.data);
+    } catch(err){
+      console.log(err);
+    }
+  }
+  
+  useEffect(()=> {
+    getMovies();
+  },[])
 
   return (
     <div className="App">
--- ReactDev/movie-gold-v1-master/src/api/axiosConfig.js
+++ ReactDev/movie-gold-v1-master/src/api/axiosConfig.js
@@ -1,9 +1,4 @@
-//import axions into project
 import axios from 'axios';
-
-
-// configure and export axios object that we use to make
-// http request to relevant remote API
 
 export default axios.create({
     baseURL:'https://9c96-103-106-239-104.ap.ngrok.io',
