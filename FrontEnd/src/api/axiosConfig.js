cb72298eaeac0fb2c101a8d32e278bc6e88da55f
Akshat Shrivastava <akshatrules04@gmail.com>
2024-01-14 14:04:38 -0500

    Made changes to ngrok forwarding link

--- FrontEnd/src/api/axiosConfig.js
+++ FrontEnd/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://9c96-103-106-239-104.ap.ngrok.io',
+    baseURL:'https://7af3-153-33-76-24.ngrok-free.app',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
